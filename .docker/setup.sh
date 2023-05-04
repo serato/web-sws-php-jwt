11e4847a9fe2e7bedae48113b594277e9b337c48
chris-die <chris@serato.com>
2023-05-05 10:28:16 +1200

    Use Serato base container image

--- .docker/setup.sh
+++ .docker/setup.sh
@@ -1,10 +1,3 @@
-# Install git an zip/unzip (required by composer)
-apt update && apt install -y git zip unzip libgmp-dev
-
-# Configure and install PHP GMP extension (this is not included in the base container images)
-docker-php-ext-configure gmp
-docker-php-ext-install gmp
-
 # Download and install Composer
 # I _could_ mount a volume and install it there. But it only takes
 # a few seconds so do it everytime the container is run.
