92dc90fa47eca89de3eb2d74cff11bc9160d393b
chris-die <chris@serato.com>
2023-05-04 23:31:57 +1200

    Defines IAccessToken interface

--- src/AccessToken.php
+++ src/AccessToken.php
@@ -3,13 +3,7 @@ declare(strict_types=1);
 
 namespace Serato\Jwt;
 
-use Serato\Jwt\Exception\InvalidSignatureException;
 use Serato\Jwt\Exception\TokenExpiredException;
-use Serato\Jwt\Exception\InvalidAudienceClaimException;
-use Serato\Jwt\Exception\InvalidSubjectClaimException;
-use Serato\Jwt\Exception\InvalidIssuerClaimException;
-use Serato\Jwt\Exception\CriticalClaimsVerificationException;
-use Serato\Jwt\Exception\UnhandledTokenCheckException;
 use Psr\Cache\CacheItemPoolInterface;
 
 /**
@@ -17,44 +11,42 @@ use Psr\Cache\CacheItemPoolInterface;
  *
  * Implements a JWS `access` token.
  */
-class AccessToken extends KmsToken
+class AccessToken extends KmsToken implements IAccessToken
 {
     private const TOKEN_CLAIM_SUB = 'access';
     private const TOKEN_SIGNING_KEY_ID = 'JWS_ACCESS_COMPACT_HS512';
     private const REFRESH_TOKEN_ID_CLAIM = 'rtid';
 
     /**
-     * Creates a JWS access token
-     *
-     * @return self
+     * @inheritdoc
     */
     final public function create(
-        string $clientAppId,
-        string $clientAppName,
-        int $clientAppAccessTokenExpirySeconds,
-        array $clientAppAccessTokenDefaultAudience,
-        string $clientAppKmsMasterKeyId,
+        string $appId,
+        string $appName,
+        int $expirySeconds,
+        array $audience,
+        string $kmsMasterKeyId,
         int $userId,
         string $userEmail,
         bool $userIsEmailVerified,
-        array $userScopesOfAccess,
+        array $scopes,
         string $refreshTokenId
-    ): self
+    ): IAccessToken
     {
         $this->createTokenWithKms(
-            $clientAppKmsMasterKeyId,
-            $clientAppId,
-            $clientAppAccessTokenDefaultAudience,
+            $kmsMasterKeyId,
+            $appId,
+            $audience,
             self::TOKEN_CLAIM_SUB,
             time(),
-            time() + $clientAppAccessTokenExpirySeconds,
+            time() + $expirySeconds,
             [
-                'app_id'            => $clientAppId,
-                'app_name'          => $clientAppName,
+                'app_id'            => $appId,
+                'app_name'          => $appName,
                 'uid'               => $userId,
                 'email'             => $userEmail,
                 'email_verified'    => $userIsEmailVerified,
-                'scopes'            => $userScopesOfAccess,
+                'scopes'            => $scopes,
                 'rtid'              => $refreshTokenId
             ],
             self::TOKEN_SIGNING_KEY_ID
@@ -63,21 +55,9 @@ class AccessToken extends KmsToken
     }
 
     /**
-     * Check the presence and validity of the claims within an access token
-     *
-     * @todo Specify void return type in PHP 7.1
-     *
-     * @param string    $webServiceUri     The URI of the validating web service
-     * @param \Memcached $memcache Memcache connection
-     *
-     * @throws TokenExpiredException
-     * @throws InvalidAudienceClaimException
-     * @throws InvalidSubjectClaimException
-     * @throws InvalidIssuerClaimException
-     * @throws CriticalClaimsVerificationException
-     * @throws UnhandledTokenCheckException
+     * @inheritdoc
      */
-    final public function validate(string $webServiceUri, \Memcached $memcache)
+    final public function validate(string $webServiceUri, \Memcached $memcache): void
     {
         $this->checkClaims($webServiceUri, self::TOKEN_CLAIM_SUB);
         $rtid = '';
@@ -94,20 +74,13 @@ class AccessToken extends KmsToken
         }
     }
 
-     /**
-     * Create the token from a JSON string and verify the token's signature
-     *
-     * @todo Specify void return type in PHP 7.1
-     *
-     * @param string                    $tokenString   Base64-encoded JWS token string
-     * @param CacheItemPoolInterface    $cache         PSR-6 cache item pool
-     *
-     * @throws InvalidSignatureException
+    /**
+     * @inheritdoc
      */
     final public function parseTokenString(
         string $tokenString,
         CacheItemPoolInterface $cache = null
-    ) {
+    ): void {
         $this->parseBase64EncodedTokenDataWithKms(
             $tokenString,
             self::TOKEN_SIGNING_KEY_ID,
---
+++ src/IAccessToken.php
@@ -0,0 +1,70 @@
+<?php
+declare(strict_types=1);
+
+namespace Serato\Jwt;
+
+use Psr\Cache\CacheItemPoolInterface;
+use Serato\Jwt\Exception\InvalidAudienceClaimException;
+use Serato\Jwt\Exception\InvalidSubjectClaimException;
+use Serato\Jwt\Exception\InvalidIssuerClaimException;
+use Serato\Jwt\Exception\CriticalClaimsVerificationException;
+use Serato\Jwt\Exception\UnhandledTokenCheckException;
+
+interface IAccessToken extends IToken {
+    /**
+     * Creates a JWS access token
+     * 
+     * @param string $appId             Client application ID
+     * @param string $appName           Client application name
+     * @param int $expirySeconds        Token expiry time in seconds
+     * @param array<string> $audience   A list of web service names that can consume the token
+     * @param string $kmsMasterKeyId    ID of KMS key used to create encryption data
+     * @param int $userId               User ID
+     * @param string $userEmail         User email address
+     * @param bool $userIsEmailVerified Indicates whether or not the user has verified their email address
+     * @param array $scopes            Scopes of access for the user
+     * @param string $refreshTokenId    ID of Refresh Token that "produces" this Access token
+     *
+     * @return self
+    */
+    public function create(
+        string $appId,
+        string $appName,
+        int $expirySeconds,
+        array $audience,
+        string $kmsMasterKeyId,
+        int $userId,
+        string $userEmail,
+        bool $userIsEmailVerified,
+        array $scopes,
+        string $refreshTokenId
+    ): self;
+
+    /**
+     * Check the presence and validity of the claims within an access token
+     *
+     * @param string    $webServiceUri     The URI of the validating web service
+     * @param \Memcached $memcache Memcache connection
+     *
+     * @throws TokenExpiredException
+     * @throws InvalidAudienceClaimException
+     * @throws InvalidSubjectClaimException
+     * @throws InvalidIssuerClaimException
+     * @throws CriticalClaimsVerificationException
+     * @throws UnhandledTokenCheckException
+     */
+    public function validate(string $webServiceUri, \Memcached $memcache): void;
+
+    /**
+     * Create the token from a JSON string and verify the token's signature
+     *
+     * @param string                    $tokenString   Base64-encoded JWS token string
+     * @param CacheItemPoolInterface    $cache         PSR-6 cache item pool
+     *
+     * @throws InvalidSignatureException
+     */
+    public function parseTokenString(
+        string $tokenString,
+        CacheItemPoolInterface $cache = null
+    ): void;
+}
